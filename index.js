2eedc8f667b93cd9e88c232cb42d618944be4fb2
maozillah <kayexmao@gmail.com>
2016-04-07 16:08:14 -0400

    using numbers for latest chart

--- index.js
+++ index.js
@@ -78,85 +78,8 @@ d3.csv("trades.csv", type, function(error, data) {
         .style("stroke-width", "1px")
         .style("opacity", "0");
 
-         // keep a reference to all our lines
+    // keep a reference to all our lines
     chartlines = document.getElementsByClassName('line');
-
-    // g.append("rect")
-    //     .attr("class", "background")
-    //     .style("pointer-events", "all")
-    //     .attr("width", width + margin.right)
-    //     .attr("height", height);
-
-     mouseG.append('svg:rect')
-    .attr('width', width)
-    .attr('height', height)
-    .attr('fill', 'none')
-    .attr('pointer-events', 'all')
-    .on('mouseout', function() { // on mouse out hide line, circles and text
-        d3.select(".mouse-line")
-            .style("opacity", "0");
-        d3.selectAll(".mouse-per-line circle")
-            .style("opacity", "0");
-        d3.selectAll(".mouse-per-line text")
-            .style("opacity", "0");
-    })
-    .on('mouseover', function() { // on mouse in show line, circles and text
-        d3.select(".mouse-line")
-            .style("opacity", "1");
-        d3.selectAll(".mouse-per-line circle")
-            .style("opacity", "1");
-        d3.selectAll(".mouse-per-line text")
-            .style("opacity", "1");
-    })
-    .on('mousemove', function() { // mouse moving over canvas
-        var mouse = d3.mouse(this);
-
-        // move the vertical line
-        d3.select(".mouse-line")
-            .attr("d", function() {
-                var d = "M" + mouse[0] + "," + height;
-                d += " " + mouse[0] + "," + 0;
-                return d;
-            });
-
-        // position the circle and text
-        d3.selectAll(".mouse-per-line")
-            .attr("transform", function(d, i) {
-                // console.log(width / mouse[0]);
-                var xDate = x.invert(mouse[0]),
-                    bisect = d3.bisector(function(d) {
-                        return d.date;
-                    }).right;
-                idx = bisect(d.values, xDate);
-
-                // since we are use curve fitting we can't relay on finding the points like I had done in my last answer
-                // this conducts a search using some SVG path functions
-                // to find the correct position on the line
-                // from http://bl.ocks.org/duopixel/3824661
-                
-                var beginning = 0,
-                    end = chartlines[i].getTotalLength(),
-                    target = null;
-
-                while (true) {
-                    target = Math.floor((beginning + end) / 2);
-                    pos = chartlines[i].getPointAtLength(target);
-                    if ((target === end || target === beginning) && pos.x !== mouse[0]) {
-                        break;
-                    }
-                    if (pos.x > mouse[0]) end = target;
-                    else if (pos.x < mouse[0]) beginning = target;
-                    else break; //position found
-                }
-
-                // update the text with y value
-                d3.select(this).select('text')
-                    .text(y.invert(pos.y).toFixed(2));
-
-                // return position
-                return "translate(" + mouse[0] + "," + pos.y + ")";
-            });
-    });
 
     lines = g.append("g")
         .each(multiple);
@@ -251,6 +174,77 @@ function multiple(dataNest) {
             .attr("class", "line")
             .attr("d", line(d.values));
     });
+
+     mouseG.append('svg:rect')
+        .attr('width', width)
+        .attr('height', height)
+        .attr('fill', 'none')
+        .attr('pointer-events', 'all')
+        .on('mouseout', function() { // on mouse out hide line, circles and text
+            d3.select(".mouse-line")
+                .style("opacity", "0");
+            d3.selectAll(".mouse-per-line circle")
+                .style("opacity", "0");
+            d3.selectAll(".mouse-per-line text")
+                .style("opacity", "0");
+        })
+        .on('mouseover', function() { // on mouse in show line, circles and text
+            d3.select(".mouse-line")
+                .style("opacity", "1");
+            d3.selectAll(".mouse-per-line circle")
+                .style("opacity", "1");
+            d3.selectAll(".mouse-per-line text")
+                .style("opacity", "1");
+        })
+        .on('mousemove', function() { // mouse moving over canvas
+            var mouse = d3.mouse(this);
+
+            // move the vertical line
+            d3.select(".mouse-line")
+                .attr("d", function() {
+                    var d = "M" + mouse[0] + "," + height;
+                    d += " " + mouse[0] + "," + 0;
+                    return d;
+                });
+
+            // position the circle and text
+            d3.selectAll(".mouse-per-line")
+                .attr("transform", function(d, i) {
+                    // console.log(width / mouse[0]);
+                    var xDate = x.invert(mouse[0]),
+                        bisect = d3.bisector(function(d) {
+                            return d.date;
+                        }).right;
+                    idx = bisect(d.values, xDate);
+
+                    // this conducts a search using some SVG path functions
+                    // to find the correct position on the line
+                    // from http://bl.ocks.org/duopixel/3824661
+
+                    var beginning = 0,
+                        end = chartlines[i].getTotalLength(),
+                        target = null;
+
+                    while (true) {
+                        var target = Math.floor((beginning + end) / 2);
+                        var pos = chartlines[i].getPointAtLength(target);
+                        if ((target === end || target === beginning) && pos.x !== mouse[0]) {
+                            break;
+                        }
+                        if (pos.x > mouse[0]) end = target;
+                        else if (pos.x < mouse[0]) beginning = target;
+                        else break; //position found
+                    }
+
+                    // update the text with y value
+                    d3.select(this).select('text')
+                        .text(y.invert(pos.y).toFixed(2));
+
+                    // return position
+                    return "translate(" + mouse[0] + "," + pos.y + ")";
+                });
+        });
+
 
 }
 
